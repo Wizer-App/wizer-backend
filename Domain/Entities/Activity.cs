0f86985bed0b4d2ab17444a18988c61f9dffbafa
Davo <omarcorreonova@gmail.com>
2025-09-30 17:18:46 -0600

    feat: add entity Team y en AppDbContext

--- Domain/Entities/Activity.cs
+++ Domain/Entities/Activity.cs
@@ -3,6 +3,8 @@ namespace Domain.Entities;
 public class Activity
 {
     //Agregar aqui no borrar lo que ya esta
+    public int? TeamId { get; set; } 
+    public Team? Team { get; set; }
     public int SchoolClassId { get; set; }
     public SchoolClass SchoolClass { get; set; } = null!;
 }
