d504c343a88a9fe9479afec21a0314fa83ef3869
RolandoGuZz <guzmancorreonova@gmail.com>
2025-09-30 19:40:46 -0600

    feat: complete DbContext

--- Domain/Entities/Activity.cs
+++ Domain/Entities/Activity.cs
@@ -2,9 +2,16 @@ namespace Domain.Entities;
 
 public class Activity
 {
-    //Agregar aqui no borrar lo que ya esta
-    public int? TeamId { get; set; } 
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime DueDate { get; set; }
+    public int CreatedById { get; set; }
+    public User CreatedBy { get; set; } = null!;
+    public int? TeamId { get; set; }
     public Team? Team { get; set; }
     public int SchoolClassId { get; set; }
     public SchoolClass SchoolClass { get; set; } = null!;
+    public ICollection<Document> Documents { get; set; } = new List<Document>();
 }
--- Domain/Entities/InfoUser.cs
+++ Domain/Entities/InfoUser.cs
@@ -4,6 +4,6 @@ public class InfoUser
 {
     public int Id { get; set; }
     public string Career { get; set; } = string.Empty;
-    public string? ProfesionalTitle { get; set; }
+    public string? ProfessionalTitle { get; set; }
     public string? Semester { get; set; }
 }
