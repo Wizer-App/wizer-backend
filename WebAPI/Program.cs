ab0cc1bc22d7420d85219664573e0a44171602bf
Davo <omarcorreonova@gmail.com>
2025-10-01 09:50:17 -0600

    fix: fix program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using Application.Interfaces;
 using Application.SchoolClasses.Handlers;
 using Application.SchoolClasses.Mapping;
+using Application.Users.Handlers;
 using Infrastructure.Data;
 using Infrastructure.Repositories;
 using MediatR;
@@ -15,10 +16,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 //los repositories
 builder.Services.AddScoped<ISchoolClassRepository, SchoolClassRepository>();
+builder.Services.AddScoped<IUserRepository, IUserRepository>();
 
 //builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly)); // Escanea Handlers en Application
+// En Program.cs
 builder.Services.AddMediatR(cfg =>
-    cfg.RegisterServicesFromAssembly(typeof(GetAllSchoolClassByUserIdHandler).Assembly));
+{
+    cfg.RegisterServicesFromAssembly(typeof(GetAllSchoolClassByUserIdHandler).Assembly);
+    cfg.RegisterServicesFromAssembly(typeof(GetAllUsersHandler).Assembly);
+    // Agrega más assemblies si tienes handlers en otros proyectos
+});
 
 builder.Services.AddAutoMapper(typeof(SchoolClassProfile)); 
 builder.Services.AddControllers();
