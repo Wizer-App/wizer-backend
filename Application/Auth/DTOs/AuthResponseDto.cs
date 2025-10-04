91f549c674032ae2c406270d68e1a84627ea7d4e
RolandoGuZz <guzmancorreonova@gmail.com>
2025-10-04 10:26:02 -0600

    feat: add Register and update Login

--- Application/Auth/DTOs/AuthResponseDto.cs
+++ Application/Auth/DTOs/AuthResponseDto.cs
@@ -1,4 +1,4 @@
-using Application.DTOs;
+namespace Application.DTOs;
 
 public class AuthResponseDto
 {
--- Application/Auth/Handlers/LoginHandler.cs
+++ Application/Auth/Handlers/LoginHandler.cs
@@ -1,5 +1,5 @@
-using System.Linq.Expressions;
 using Application.Auth.Commands;
+using Application.Users.Queries;
 using Application.DTOs;
 using MediatR;
 using Supabase;
@@ -7,16 +7,29 @@ using Supabase;
 public class LoginHandler : IRequestHandler<LoginCommand, AuthResponseDto>
 {
     private readonly Client _client;
-    public LoginHandler(Client client)
+    private readonly IMediator _mediator;
+    public LoginHandler(Client client, IMediator mediator)
     {
         _client = client;
+        _mediator = mediator;
     }
     public async Task<AuthResponseDto> Handle(LoginCommand request, CancellationToken cancellationToken)
     {
         try
         {
             var response = request.LoginDto;
-            var session = await _client.Auth.SignIn(response.Username, response.Password);
+            var getUserQuery = new GetByUsernameQuery(response.Username);
+            var user = await _mediator.Send(getUserQuery, cancellationToken);
+            if (user == null)
+            {
+                return new AuthResponseDto
+                {
+                    Message = "Error, Usuario no encontrado",
+                    Success = false
+                };
+            }
+
+            var session = await _client.Auth.SignIn(user.Email, response.Password);
             if (session?.User?.Id == null)
             {
                 return new AuthResponseDto
@@ -38,12 +51,12 @@ public class LoginHandler : IRequestHandler<LoginCommand, AuthResponseDto>
                 };
             }
         }
-        catch (Exception e)
+        catch (Exception)
         {
             return new AuthResponseDto
             {
                 Success = false,
-                Message = e.Message
+                Message = "Credenciales invalidas"
             };
         }
     }
